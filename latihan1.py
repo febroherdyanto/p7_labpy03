b41729e6d8bf838e1b4dd419df3b9d797e11ebe1
Febro Herdyanto <febroherdyanto@outlook.com>
2020-11-07 19:31:44 +0700

    Update Python File

---
+++ latihan1.py
@@ -0,0 +1,10 @@
+#File ini digunakan untuk Tugas Latihan 1 pada materi Praktikum 3
+
+print("Masukkan Nilai N : ")
+n=int(input())
+
+import random
+print("Random() : ", random.randrange())
+
+for x in list(range(1,n+1,1)):
+    print("Data ke: ",x,"=>")
