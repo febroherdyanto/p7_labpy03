914db213d90beedabc02f76ad07583461edb9068
Febro Herdyanto <febroherdyanto@outlook.com>
2020-11-07 20:49:14 +0700

    Update File Python Program 1

--- program1.py
+++ program1.py
@@ -4,6 +4,12 @@ print("Tugas Pertemuan 7 - Praktikum 3 - Program 1 \n")
 
 modalawal = 100000000
 
-for x (1,9,1):
-    #Menentukan Laba
-    if(x>=1 and x<=2)
+for bulan in range(1,9,1):
+    #Menentukan Laba pada bulan ke 1 dan ke 2
+    if(bulan>=1 and bulan<=2):
+        #laba bulan 1 dan 2
+        laba=modalawal*0
+        print(f"Laba bulan ke {bulan} : {laba}")
+    else : print("")
+
+    if(bulan>=3 and bulan<=4):
